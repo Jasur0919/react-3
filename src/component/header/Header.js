5390cce645329b6a9166558a1baae59090085ccc
Jasur0919 <jasurjohn1111@gmail.com>
2024-04-07 11:13:29 +0500

    ihfdfgruhsnskd

---
+++ src/component/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <h2>Admin</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam neque odio tenetur odit eveniet vitae magni totam, sint quae eius officia tempora fugiat voluptatum explicabo minus? Accusamus numquam laborum eius.</p>
+    </div>
+  )
+}
+
+export default Header
